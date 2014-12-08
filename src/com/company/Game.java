e6eb474b7105273c8c8ca6f68a668bcaddca4514
nrakochy <nick.rakochy@gmail.com>
2014-12-08 13:26:13 -0600

    Testing Green for bowling Spare. Not yet refactored. Slide 42 in ppt

---
+++ src/com/company/Game.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/**
+ * Created by user on 12/8/14.
+ */
+public class Game {
+    private int score = 0;
+    private int rolls[] = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins){
+        score += pins;
+        rolls[currentRoll ++] = pins;
+    }
+    public int score() {
+        int score = 0;
+        int i = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (rolls[i] + rolls[i + 1] == 10) {
+                score += 10 + rolls[i + 2];
+                i += 2;
+            } else {
+                score += rolls[i] + rolls[i + 1];
+                i += 2;
+            }
+        }
+            return score;
+        }
+}
---
+++ test/com/company/GameTest.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import junit.framework.TestCase;
+
+public class GameTest extends TestCase {
+    private Game g;
+
+    protected void setUp() throws Exception{
+            g = new Game();
+    }
+
+    private void rollMany(int n, int pins){
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+
+    public void testGutterGame() throws Exception{
+        rollMany(20, 0);
+        assertEquals(0, g.score());
+    }
+
+    public void testAllOnes() throws Exception{
+        rollMany(20, 1);
+        assertEquals(20, g.score());
+    }
+
+    public void testOneSpare() throws Exception{
+        g.roll(5);
+        g.roll(5); //spare
+        g.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, g.score());
+    }
+}
