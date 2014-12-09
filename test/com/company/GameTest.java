618a56d5da27e273e951871d5fcbe0ac52157183
nrakochy <nick.rakochy@gmail.com>
2014-12-09 09:00:49 -0600

    Refactored isSpare method to remove comments and clarify code intent

--- test/com/company/GameTest.java
+++ test/com/company/GameTest.java
@@ -25,10 +25,14 @@ public class GameTest extends TestCase {
     }
 
     public void testOneSpare() throws Exception{
-        g.roll(5);
-        g.roll(5); //spare
+        rollSpare();
         g.roll(3);
         rollMany(17, 0);
         assertEquals(16, g.score());
+    }
+
+    private void rollSpare(){
+        g.roll(5);
+        g.roll(5);
     }
 }
